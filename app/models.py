c307fd1bef814535e5dedff213c9fa4d25ec197c
JeromeMberia <jeromemberia@gmail.com>
2020-05-11 02:59:50 +0300

    creeting models

--- app/models.py
+++ app/models.py
@@ -1,2 +1,91 @@
+from flask import current_app
+from flask_login import UserMixin
+from . import db, login_manager
+from _datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Source :
+    '''
+    class to define each source object
+    '''
+    def __init__(self,author,quote):
+        self.author = author
+        self.quote = quote
+        
+class Post(db.Model):
+
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(100), nullable=False)
+
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    content = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+    
+
+class User(db.Model, UserMixin):
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    password = db.Column(db.String(60), nullable=False)
+    
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Comment(db.Model):
+    
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    comment = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    pitchs_id = db.Column(db.Integer, db.ForeignKey("pitchs.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comment(cls,id):
+        comment = Comment.query.filter_by(pitchs_id=id).all()
+        return comment
 
 
